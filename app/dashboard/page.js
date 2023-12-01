46c098e6ab94c4fa4f7f93b848ff07a0975ba119
PADMAPRIYA <padmapriyas.2004@gmail.com>
2023-12-01 22:33:14 +0530

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -15,7 +15,7 @@ const Dashboard = () => {
 
     const fetchData = async () => {
       try {
-        const response = await fetch('https://digitized-work-tracker-backend-huem4aror-vishal-marvel.vercel.app/api/v1/books/statistics-for-date');
+        const response = await fetch('https://digitized-work-tracker-backend.vercel.app/api/v1/books/statistics-for-date');
         const data = await response.json();
        
         setBooksScanned(data.booksScannedToday);
